8928fca30dab52caff1bad5f83d489fa1474af73
erikvalentim <eriklenda1@gmail.com>
2025-07-25 17:21:54 +0000

    Aula 25_07 SearchProducts

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,15 +1,37 @@
 import styles from "./ProductList.module.css";
 import { CircularProgress } from "@mui/material";
 import { Product } from "./Product";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { CartContext } from "../service/CartContext";
 
 export function ProductList() {
   
   const { products, loading, error } = useContext(CartContext);
+  const searchInput = useRef(null);
 
+  function handleClear() {
+    searchInput.current.value = "";
+  }
+ function handleSearch() {
+    const searchTerm = searchInput.current.value.toLowerCase();
+    console.log(searchTerm);
+    
+ }
+  
   return (
     <div className={styles.container}>
+      <div className="{styles.searchDiv}">
+      <input
+        type="text"
+        ref={searchInput}
+        placeholder="Search products..."
+
+        className={styles.searchInput}
+        onChange={handleSearch}
+
+     />
+     <button className={styles.searchButton} onClick={handleClear}>CLEAR</button>
+     </div>
       <div className={styles.grid}>
         {products.map((product) => (
           <Product key={product.id} product={product} />
